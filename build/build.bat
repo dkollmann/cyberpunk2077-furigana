ebc648e3b327c7409ee362fed21ea9ffd3d16e1c
Daniel Kollmann <dankolle@mail.de>
2022-01-19 15:39:39 +0100

    Added dist folder to build process.

--- build/build.bat
+++ build/build.bat
@@ -6,6 +6,14 @@ set CP2077=C:\Program Files (x86)\GOG Galaxy\Games\Cyberpunk 2077
 rem Run the compiler
 echo Cyberpunk 2077 Path: %CP2077%
 
+echo Copying files...
+mkdir ..\dist\r6\scripts
+copy /y ..\src\redscript\* ..\dist\r6\scripts\*
+
+echo Copy to CP2077 folder...
+xcopy /y /e ..\dist\* "%CP2077%"
+
+echo Running redscript compiler...
 "%~dp0\redscript-cli.exe" compile -s "%CP2077%\r6\scripts" -b "%CP2077%\r6\cache\final.redscripts" -o "%CP2077%\r6\cache\final_patched.redscripts"
 
 rem type "%CP2077%\r6\cache\redscript.log"
