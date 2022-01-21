a4c86acf5869159eeef521e580ac7f8c26d167c2
Daniel Kollmann <dankolle@mail.de>
2022-01-21 23:55:49 +0100

    Finished batchfile to update the compiled subtitles.

--- build/import_subtitles.bat
+++ build/import_subtitles.bat
@@ -2,4 +2,21 @@
 
 cd %~dp0
 
-WolvenKit.Console\WolvenKit.CLI.exe cr2w -d -p "..\src\wolvenkit\Cyberpunk 2077 Furigana\files\Raw_Subtitles"
+set SUBTITLEPATH=base\localization\jp-jp\subtitles
+set SOURCE=..\src\wolvenkit\Cyberpunk 2077 Furigana\files\Raw_Subtitles\%SUBTITLEPATH%
+set TARGET=..\src\wolvenkit\Cyberpunk 2077 Furigana\files\Mod\%SUBTITLEPATH%
+
+echo Removing previous files...
+rmdir /s/q "%TARGET%"
+mkdir "%TARGET%"
+
+echo Copying files...
+xcopy /s /q "%SOURCE%" "%TARGET%"
+
+echo Converting files...
+call WolvenKit.Console\WolvenKit.CLI.exe cr2w -d -p "%TARGET%"
+color 07
+
+echo Deleting copied source files...
+del /s /q "%TARGET%\*.json.json" > __killme__
+del /q __killme__
