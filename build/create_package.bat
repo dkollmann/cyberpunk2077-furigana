0f39420b9f8ca4da38272792f90257e5d75da170
Daniel Kollmann <dankolle@mail.de>
2022-02-04 18:59:20 +0100

    The package now contains the release dll.

--- build/create_package.bat
+++ build/create_package.bat
@@ -6,4 +6,7 @@ del /q %ARCHIVE%
 
 mkdir ..\packages
 
+rem Use release DLL
+copy /y ..\src\red4ext\x64\Release\*.dll ..\dist\red4ext\plugins\*
+
 "C:\Program Files\WinRAR\Rar.exe" a -r -ep1 -m5 -t -x..\dist\bin\x64\plugins\cyber_engine_tweaks\mods\nativeSettings %ARCHIVE% ..\dist\*
