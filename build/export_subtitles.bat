51436f24ad9f8052098264efd2bbeb5636269a3a
Daniel Kollmann <dankolle@mail.de>
2023-09-29 16:17:04 +0900

    Exported subtitles for CP2077 v2.0 and Phantom Liberty.

--- build/export_subtitles.bat
+++ build/export_subtitles.bat
@@ -5,12 +5,14 @@ cd %~dp0
 set BUILDFOLDER=%CD%
 call cp2077path.bat
 set SUBTITLEPATH=base\localization\jp-jp\subtitles
+set SUBTITLEPATH_EP1=ep1\localization\jp-jp\subtitles
 SET WOLVENKITFILES=%BUILDFOLDER%\..\src\wolvenkit\Cyberpunk 2077 Furigana\files
 SET MODFILES=%WOLVENKITFILES%\Mod_Exported
 SET RAWFILES=%WOLVENKITFILES%\Raw
-set SOURCE=%MODFILES%\%SUBTITLEPATH%
-set TARGETRAW=%RAWFILES%\%SUBTITLEPATH%
+set SOURCE=%MODFILES%
+set TARGETRAW=%RAWFILES%
 set ARCHIVEFOLDER=%CP2077FOLDER%\archive\pc\content
+set ARCHIVEFOLDER_EP1=%CP2077FOLDER%\archive\pc\ep1
 
 rmdir /s/q "%TARGETRAW%"
 mkdir "%TARGETRAW%"
@@ -23,10 +25,12 @@ mkdir "%SOURCE%"
 echo Exporting subtitles (%ARCHIVEFOLDER%)...
 cd /d "%ARCHIVEFOLDER%"
 
-for /r %%i in (*.archive) do (
-	echo   %%i
-	call "%BUILDFOLDER%\WolvenKit.Console\WolvenKit.CLI.exe" unbundle -p "%%i" -o "%MODFILES%" -w "%SUBTITLEPATH%\*"
-)
+call "%BUILDFOLDER%\WolvenKit.Console\WolvenKit.CLI.exe" unbundle -p lang_ja_text.archive -o "%MODFILES%" -w "%SUBTITLEPATH%\*"
+
+echo Exporting subtitles (%ARCHIVEFOLDER_EP1%)...
+cd /d "%ARCHIVEFOLDER_EP1%"
+
+call "%BUILDFOLDER%\WolvenKit.Console\WolvenKit.CLI.exe" unbundle -p lang_ja_text.archive -o "%MODFILES%" -w "%SUBTITLEPATH_EP1%\*"
 
 color 07
 cd /d "%BUILDFOLDER%"
